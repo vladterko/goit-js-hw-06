e1ff39b47b1f48be70a792ef8d31fa3814705614
Vlad Tereshchenko <vlad.terko@gmail.com>
2022-10-26 17:45:41 +0300

    Fix 05 task

--- js/task-05.js
+++ js/task-05.js
@@ -5,19 +5,9 @@ console.log(inputName.value);
 
 const onInputName = event => {
     outputName.textContent = event.currentTarget.value;
-};
-
-const onEmptyNameField = () => {
     if (inputName.value === "") {
-        inputName.value = "Anonymous";
         outputName.textContent = "Anonymous";
-        inputName.addEventListener('focus', () => {
-            if (inputName.value === "Anonymous") {
-                inputName.value = "";
-            }
-        });
     }
 };
 
 inputName.addEventListener('input', onInputName);
-inputName.addEventListener('blur', onEmptyNameField);
